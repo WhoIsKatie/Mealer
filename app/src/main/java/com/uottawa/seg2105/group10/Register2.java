f75edf2cafb4a50bdd46ee1b16a07ae4d86f5b51
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-10-02 03:03:26 -0400

    Package Statement

---
+++ app/src/main/java/com/uottawa/seg2105/group10/Register2.java
@@ -0,0 +1,14 @@
+package com.uottawa.seg2105.group10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
+}
