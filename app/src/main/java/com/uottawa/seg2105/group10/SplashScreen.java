7d3dc1d70a6c4accde2a83b4e61f8f28edead6c2
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-10-01 22:01:49 -0400

    Package Statement

---
+++ app/src/main/java/com/uottawa/seg2105/group10/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.uottawa.seg2105.group10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
