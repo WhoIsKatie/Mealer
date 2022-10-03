ac314dcd6b9fe888ce076b0317436c0d82d7d7c1
Elsa Ko <eko095@uottawa.ca>
2022-10-02 22:40:26 -0400

    fixed broken register button (by changing from fullscreen to empty activity) new register is called register1.

--- app/src/main/java/com/uottawa/seg2105/group10/Login.java
+++ app/src/main/java/com/uottawa/seg2105/group10/Login.java
@@ -2,13 +2,28 @@ package com.uottawa.seg2105.group10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button registrationButt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registrationButt2 = findViewById(R.id.registrationButt2);
+        registrationButt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register1.class));
+            }
+        });
     }
+
+
 }
