797758d04ffb12fdbf1f772c5c8880e9a07b69cd
Tess <tharp083@uottawa.ca>
2022-10-06 16:29:58 -0400

    update register1.java

--- app/src/main/java/com/uottawa/seg2105/group10/ui/Register1.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/Register1.java
@@ -11,27 +11,43 @@ import com.uottawa.seg2105.group10.R;
 
 public class Register1 extends AppCompatActivity {
 
+
+    //Initializing buttons
     private Button cookOption, clientOption;
-    public boolean role;
+
+
+    //if cook == true than you are a cook
+    //if cook == false than you are a client
+    public boolean cook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
 
+        //creating option based of off pulled id's
         cookOption = findViewById(R.id.cookOption);
-        cookOption.setOnClickListener(new View.OnClickListener() {
+        clientOption = findViewById(R.id.clientOption);
 
+
+
+        //cook button in register1 that directs you to register2
+        //when cook button is clicked cook is set  to ture
+        cookOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                cook = true;
                 startActivity(new Intent(Register1.this, Register2.class));
             }
         });
 
-        clientOption = findViewById(R.id.clientOption);
+
+        //client button in register1 that directs you to register2
+        //when client button is clicked cook is set to false
         clientOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                cook = false;
                 startActivity(new Intent(Register1.this, Register2.class));
             }
         });
