b230779d1a298c8a35bc77a4e1b24a0c11dfa4d5
Katie Liu <katieliu505@gmail.com>
2022-10-06 14:30:50 -0400

    connected to Firebase in build.gradle files

--- app/src/main/java/com/uottawa/seg2105/group10/Register1.java
+++ app/src/main/java/com/uottawa/seg2105/group10/Register1.java
@@ -1,14 +1,35 @@
 package com.uottawa.seg2105.group10;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Register1 extends AppCompatActivity {
+
+    private Button cookOption, clientOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
+
+        cookOption = findViewById(R.id.cookOption);
+        cookOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register1.this, Register2.class));
+            }
+        });
+
+        clientOption = findViewById(R.id.clientOption);
+        clientOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register1.this, Register2.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/uottawa/seg2105/group10/Register2.java
+++ app/src/main/java/com/uottawa/seg2105/group10/Register2.java
@@ -1,10 +1,12 @@
 package com.uottawa.seg2105.group10;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Register2 extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
