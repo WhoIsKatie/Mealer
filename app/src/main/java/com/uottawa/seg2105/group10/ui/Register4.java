d789f6c2cb5590d62bea9553c64dc6021775d2b6
Tess <tharp083@uottawa.ca>
2022-10-13 16:41:33 -0400

    hopfully made an void check image button

--- app/src/main/java/com/uottawa/seg2105/group10/ui/Register4.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/Register4.java
@@ -1,11 +1,16 @@
 package com.uottawa.seg2105.group10.ui;
 
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -17,10 +22,13 @@ public class Register4 extends AppCompatActivity {
     private Button nextButt;
     private Button login;
     private ImageButton back;
-
+    private Button gal;
     private TextInputEditText profile;
+    private  ImageView check;
+    private final int GALLERY_REQ_CODE = 1000;
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,8 @@ public class Register4 extends AppCompatActivity {
         nextButt = findViewById(R.id.signup_submit_button);
         login = findViewById(R.id.signup_login_button2);
         back = findViewById(R.id.BackButFOrReg4);
+        check = findViewById(R.id.check);
+        gal = findViewById(R.id.galery);
         nextButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,6 +71,31 @@ public class Register4 extends AppCompatActivity {
             }
 
         });
+
+        gal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+
+            if (resultCode == GALLERY_REQ_CODE){
+                //For gallery
+                check.setImageURI(data.getData());
+            }
+        }
 
     }
 }
