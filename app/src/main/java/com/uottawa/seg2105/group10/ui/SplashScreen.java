5169bce7c4f6ce98b91ced07f4b591e5c792d5b1
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-10-14 17:56:32 -0400

    Unimportant modifications

--- app/src/main/java/com/uottawa/seg2105/group10/ui/SplashScreen.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/SplashScreen.java
@@ -1,8 +1,8 @@
 package com.uottawa.seg2105.group10.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.uottawa.seg2105.group10.R;
 
