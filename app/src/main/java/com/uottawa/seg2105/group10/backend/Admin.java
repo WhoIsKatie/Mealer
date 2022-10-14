d6e02fe39423f0c97e9c6fde17af27a9ff57d79c
sarankimahendran <saranki.mahendran@gmail.com>
2022-10-14 10:33:34 -0400

    i finally committed :)))) [Admin.java]

---
+++ app/src/main/java/com/uottawa/seg2105/group10/backend/Admin.java
@@ -0,0 +1,19 @@
+package com.uottawa.seg2105.group10.backend;
+
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Admin{
+
+    String [] inbox; //Complaints from Clients
+    String [] suspendComplaints;
+    String [] acceptComplaints;
+
+
+    ///matching super class
+    public Admin() {
+        String email = "jacobmaurice2003@gmail.com";
+        String password = "admin123";
+
+    }
+}
