e394a5331887256a30959aa5c4f3780f92bffce0
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-11-06 15:30:35 -0500

    ComplaintView now shows the Time and name of Cook properly

--- app/src/main/java/com/uottawa/seg2105/group10/temp/ComplaintModel.java
+++ app/src/main/java/com/uottawa/seg2105/group10/temp/ComplaintModel.java
@@ -4,35 +4,36 @@ import com.google.firebase.firestore.DocumentSnapshot;
 
 public class ComplaintModel {
 
-    String nameOfCook;
+    String nameOfClient;
     String timeOfComplaint;
     String titleOfComplaint;
     String descriptionOfComplaint;
+    String cookName;
     String cookUid, clientUid;
     private DocumentSnapshot document;
 
 
-    public ComplaintModel(String nameOfCook, String timeOfComplaint, String titleOfComplaint, String descriptionOfComplaint, String cookUid, String clientUid) {
-        this.nameOfCook = nameOfCook;
+    public ComplaintModel(String nameOfClient, String timeOfComplaint, String titleOfComplaint, String descriptionOfComplaint, String cookName, String clientUid) {
+        this.nameOfClient = nameOfClient;
         this.timeOfComplaint = timeOfComplaint;
         this.titleOfComplaint = titleOfComplaint;
         this.descriptionOfComplaint = descriptionOfComplaint;
-        this.cookUid = cookUid;
+        this.cookName = cookName;
         this.clientUid = clientUid;
     }
     public DocumentSnapshot getDocument(){
         return document;
     }
 
-    public String getNameOfCook() {
-        return nameOfCook;
+    public String getNameOfClient() {
+        return nameOfClient;
     }
     public String getTimeOfComplaint() {
         return timeOfComplaint;
     }
     public String getTitleOfComplaint(){return titleOfComplaint;}
     public String getDescriptionOfComplaint(){return descriptionOfComplaint;}
-    public String getCookUid(){return cookUid;}
+    public String getCookName(){return cookName;}
     public String getClientUid(){return clientUid;}
 }
 
--- app/src/main/java/com/uottawa/seg2105/group10/ui/AdminHome.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/AdminHome.java
@@ -55,11 +55,11 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
         //TODO: add new field to complaints for status: active, tempSuspend, indefSuspend, dismssed
 
         // initializing all lists of fields for complaints that are active
-        ArrayList<String> cookName = new ArrayList<>();
+        ArrayList<String> clientName = new ArrayList<>();
         ArrayList<String> descriptionOfComplaint = new ArrayList<>();
         ArrayList<String> titleOfComplaint = new ArrayList<>();
         ArrayList<String> timeOfComplaint = new ArrayList<>();
-        ArrayList<String> cookUid = new ArrayList<>();
+        ArrayList<String> cookName = new ArrayList<>();
         ArrayList<String> clientUid = new ArrayList<>();
 
         dBase.collection("complaints").whereEqualTo("status", true).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -68,15 +68,15 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
                 for(DocumentSnapshot document : queryDocumentSnapshots.getDocuments()){
                     Log.d(TAG, document.getId() + "=>" + document.getData());
                     Map<String, Object> data = document.getData();
-                    cookName.add(data.get("cookName").toString());
+                    clientName.add(data.get("clientName").toString());
                     titleOfComplaint.add(data.get("title").toString());
                     descriptionOfComplaint.add(data.get("description").toString());
                     timeOfComplaint.add(data.get("time").toString());
-                    cookUid.add(data.get("cookUid").toString());
+                    cookName.add(data.get("cookName").toString());
                     clientUid.add(data.get("clientUid").toString());
                 }
                 for (int i = 0; i < titleOfComplaint.size(); i++){
-                    ComplaintModel cm = new ComplaintModel(cookName.get(i),timeOfComplaint.get(i), titleOfComplaint.get(i), descriptionOfComplaint.get(i), cookUid.get(i), clientUid.get(i));
+                    ComplaintModel cm = new ComplaintModel(clientName.get(i),timeOfComplaint.get(i), titleOfComplaint.get(i), descriptionOfComplaint.get(i), cookName.get(i), clientUid.get(i));
                     complaintModel.add(cm);
                 }
                 updateView();
@@ -84,30 +84,17 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
 
 
         });
-
-        //TODO: delete strings => iterate over database complaints collection
-        /*
-        String[] namesOfUsers = getResources().getStringArray(R.array.names_of_unsatisfied_customers);
-        String[] timesOfComplaint = getResources().getStringArray(R.array.times_of_complaint);
-        String[] titleOfComplaint = getResources().getStringArray(R.array.title_of_complaint);
-        String[] descriptionOfComplaint = getResources().getStringArray(R.array.description_of_complaint);
-        String[] cookOfComplaint = getResources().getStringArray(R.array.cook_concerned_by_complaint);
-
-        for (int i = 0; i < namesOfUsers.length; i++){
-            complaintModel.add(new ComplaintModel(namesOfUsers[i], timesOfComplaint[i], titleOfComplaint[i], descriptionOfComplaint[i], cookOfComplaint[i], "waoesdfkjf"));
-        }
-         */
     }
 
     @Override
     public void onItemClick(int position) {
         Intent intent = new Intent(AdminHome.this, ComplaintView.class);
 
-        intent.putExtra("NAME", complaintModel.get(position).getNameOfCook());
+        intent.putExtra("NAME", complaintModel.get(position).getNameOfClient());
         intent.putExtra("TIME", complaintModel.get(position).getTimeOfComplaint());
         intent.putExtra("TITLE", complaintModel.get(position).getTitleOfComplaint());
         intent.putExtra("DESCRIPTION", complaintModel.get(position).getDescriptionOfComplaint());
-        intent.putExtra("COOK", complaintModel.get(position).getCookUid());
+        intent.putExtra("COOK", complaintModel.get(position).getCookName());
         intent.putExtra("CLIENT", complaintModel.get(position).getClientUid());
 
         startActivity(intent);
