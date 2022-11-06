375f29767373651b0bebee556a040f4f04e1f80c
Elsa Ko <eko095@uottawa.ca>
2022-11-06 18:13:03 -0500

    suspend and dismiss working. suspend time is temporarily hard-coded, will change when we get time.

--- app/src/main/java/com/uottawa/seg2105/group10/ui/AdminHome.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/AdminHome.java
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -62,7 +61,7 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
         ArrayList<String> timeOfComplaint = new ArrayList<>();
         ArrayList<String> cookUid = new ArrayList<>();
         ArrayList<String> clientUid = new ArrayList<>();
-        ArrayList<DocumentReference> documents = new ArrayList<>();
+        ArrayList<String> documents = new ArrayList<>();
 
         dBase.collection("complaints").whereEqualTo("status", true).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
@@ -76,7 +75,7 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
                     timeOfComplaint.add(data.get("time").toString());
                     cookUid.add(data.get("cookUid").toString());
                     clientUid.add(data.get("clientUid").toString());
-                    documents.add(document.getReference());
+                    documents.add(document.getReference().getId());
                 }
                 for (int i = 0; i < titleOfComplaint.size(); i++){
                     ComplaintModel cm = new ComplaintModel(documents.get(i), cookName.get(i),timeOfComplaint.get(i), titleOfComplaint.get(i), descriptionOfComplaint.get(i), cookUid.get(i), clientUid.get(i));
@@ -105,13 +104,14 @@ public class AdminHome extends AppCompatActivity implements RecyclerViewInterfac
     @Override
     public void onItemClick(int position) {
         Intent intent = new Intent(AdminHome.this, ComplaintView.class);
-        intent.putExtra("DOCUMENT", complaintModel.get(position));
-        /*intent.putExtra("NAME", complaintModel.get(position).getNameOfCook());
-        intent.putExtra("TIME", complaintModel.get(position).getTimeOfComplaint());
-        intent.putExtra("TITLE", complaintModel.get(position).getTitleOfComplaint());
-        intent.putExtra("DESCRIPTION", complaintModel.get(position).getDescriptionOfComplaint());
-        intent.putExtra("COOK", complaintModel.get(position).getCookUid());
-        intent.putExtra("CLIENT", complaintModel.get(position).getClientUid());*/
+        ComplaintModel doc = complaintModel.get(position);
+        intent.putExtra("DOCUMENT", doc.getDocID());
+        intent.putExtra("NAME", doc.getNameOfCook());
+        intent.putExtra("TIME", doc.getTimeOfComplaint());
+        intent.putExtra("TITLE", doc.getTitleOfComplaint());
+        intent.putExtra("DESCRIPTION", doc.getDescriptionOfComplaint());
+        intent.putExtra("COOK", doc.getCookUid());
+        intent.putExtra("CLIENT", doc.getClientUid());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/uottawa/seg2105/group10/ui/ComplaintView.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/ComplaintView.java
@@ -4,25 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.uottawa.seg2105.group10.R;
-import com.uottawa.seg2105.group10.temp.ComplaintModel;
 
 public class ComplaintView extends AppCompatActivity {
 
 
     private DocumentSnapshot document;
     //Intializing butons
-    private Button dismissButt;
+    private Button dismissButt, suspendButt;
+    private RadioGroup suspendTimes;
+    private RadioButton fiveday, tenday, indef;
 
     private FirebaseFirestore dBase;
 
@@ -32,12 +32,14 @@ public class ComplaintView extends AppCompatActivity {
         setContentView(R.layout.activity_complaintview);
         dBase = dBase.getInstance();
 
-        /*String name = getIntent().getStringExtra("NAME");
+        String name = getIntent().getStringExtra("NAME");
         String time = getIntent().getStringExtra("TIME");
         String title = getIntent().getStringExtra("TITLE");
         String description = getIntent().getStringExtra("DESCRIPTION");
-        String cook = getIntent().getStringExtra("COOK");*/
-        ComplaintModel complaint = getIntent().getParcelableExtra("DOCUMENT");
+        String cook = getIntent().getStringExtra("COOK");
+        String docID = getIntent().getStringExtra("DOCUMENT");
+
+        DocumentReference docRef = dBase.collection("complaints").document(docID);
 
         TextView nameTextView = findViewById(R.id.textView9);
         TextView timeTextView = findViewById(R.id.textView10);
@@ -45,27 +47,51 @@ public class ComplaintView extends AppCompatActivity {
         TextView descriptionTextView = findViewById(R.id.textView13);
         TextView cookTextView = findViewById(R.id.textView16);
 
-        nameTextView.setText(complaint.getNameOfCook());
-        timeTextView.setText(complaint.getTimeOfComplaint());
-        titleTextView.setText(complaint.getTitleOfComplaint());
-        descriptionTextView.setText(complaint.getDescriptionOfComplaint());
-        cookTextView.setText(complaint.getCookUid());
+        nameTextView.setText(name);
+        timeTextView.setText(time);
+        titleTextView.setText(title);
+        descriptionTextView.setText(description);
+        cookTextView.setText(cook);
         dismissButt = findViewById(R.id.dismissButt);
+        suspendButt = findViewById(R.id.suspendButt);
 
         dismissButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DocumentReference docRef = complaint.getDocument();
-                Task task = docRef.update("status", false);
-                task.addOnCompleteListener(new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        startActivity(new Intent(ComplaintView.this, AdminHome.class));
-                    }
-                });
+                docRef.update("status", false);
+                startActivity(new Intent(ComplaintView.this, ComplaintView.class));
+                finish();
             }
         });
 
+        suspendButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docRef.update("suspended", true);
+                docRef.update("releaseDays", 10);
+                //TODO: change how release days is done!!
+                docRef.update("status", false);
+
+                //TODO: set how long suspension is for.
+                //suspendTimes.setVisibility(View.VISIBLE);
+                /*fiveday.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        DateTimeFormatter dtf = null;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                            LocalDateTime now = LocalDateTime.now();
+                            //dtf.format(now);
+                            docRef.update("releaseDate", dtf.format(now));
+                        }
+                        startActivity(new Intent(ComplaintView.this, ComplaintView.class));
+                        finish();
+                    }
+                }); */
+                startActivity(new Intent(ComplaintView.this, ComplaintView.class));
+                finish();
+            }
+        });
 
     }
 }
