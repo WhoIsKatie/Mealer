64f450062dda9219048bd68717d1bd4ec985ec7b
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-11-07 21:29:36 -0500

    Menu1 and meal card view xml layout files completed

---
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/Menu1.java
@@ -0,0 +1,16 @@
+package com.uottawa.seg2105.group10.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uottawa.seg2105.group10.R;
+
+public class Menu1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+    }
+}
