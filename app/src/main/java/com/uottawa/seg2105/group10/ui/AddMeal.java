7ad5072a5edc2b58ad2e8d8d8caee6debf01644b
Elsa Ko <eko095@uottawa.ca>
2022-11-09 18:48:30 -0500

    Finished up AddMeal xml

--- app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
@@ -1,9 +1,11 @@
-/*
-package com.uottawa.seg2105.group10;
+
+package com.uottawa.seg2105.group10.ui;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.uottawa.seg2105.group10.R;
 
 public class AddMeal extends AppCompatActivity {
 
@@ -12,4 +14,4 @@ public class AddMeal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
     }
-}*/
+}
