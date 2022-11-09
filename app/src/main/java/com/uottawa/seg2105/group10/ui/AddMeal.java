cfd582d321ab649d1446ccd1730c9797dd02bd81
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-11-09 17:14:46 -0500

    AddMeal activity added

--- app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
@@ -1,17 +1,15 @@
 /*
-package com.uottawa.seg2105.group10.ui;
+package com.uottawa.seg2105.group10;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.uottawa.seg2105.group10.R;
-
-public class AllMealsMenu extends AppCompatActivity {
+public class AddMeal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_meals_menu);
+        setContentView(R.layout.activity_add_meal);
     }
 }*/
