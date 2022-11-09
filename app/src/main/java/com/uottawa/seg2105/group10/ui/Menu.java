3adea8273f1812f07e2a632fff80b8be93a72211
JacobMaurice-jm <jacob.maurice.jm@gmail.com>
2022-11-09 02:29:55 -0500

    XML files completed (Deliverable 3)

--- app/src/main/java/com/uottawa/seg2105/group10/ui/Menu.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/Menu.java
@@ -1,16 +1,3 @@
 package com.uottawa.seg2105.group10.ui;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.uottawa.seg2105.group10.R;
-
-public class Menu extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
-}
+public class Menu {}
