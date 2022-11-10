161f2b8e30c59c6390a36a2cabe1e6825b43ab7c
Elsa Ko <eko095@uottawa.ca>
2022-11-10 16:11:34 -0500

    Utility class for uploading photos created and register 4 and add meal have been suitably adapted to use the utility class. Untested on emulator because homepage next button has not been configured to go to the meal activity.

--- app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
+++ app/src/main/java/com/uottawa/seg2105/group10/ui/AddMeal.java
@@ -1,17 +1,61 @@
 
 package com.uottawa.seg2105.group10.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
 import com.uottawa.seg2105.group10.R;
+import com.uottawa.seg2105.group10.backend.Utility;
 
 public class AddMeal extends AppCompatActivity {
+    private Uri filePath;
+    private ImageView mealImage;
+    private FirebaseAuth mAuth;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
+        Button removeButt, save;
+        EditText mealName, mealDesc, mealPrice;
+        mealImage = findViewById(R.id.mealImage);
+        Button changePicture = findViewById(R.id.changePicture);
+        mAuth = FirebaseAuth.getInstance();
+
+        changePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1000){
+                filePath = data.getData();
+                mealImage.setImageURI(filePath);
+            }
+            Utility util = new Utility(AddMeal.this, filePath, mAuth, storage);
+            util.uploadImage();
+        }
     }
 }
